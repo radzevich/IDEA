7f7dff6b2dab015d29365c7a8bef3565da6f8e6f
Alex Radevich <virtuain98@gmail.com>
2016-10-30 19:07:38 +0300

    Keys set initializing added

--- KeyGen.cpp
+++ KeyGen.cpp
@@ -2,34 +2,24 @@
 #include <io.h>
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 #include "Error.h"
 
-namespace KEYS
+namespace Keys
 {
 	void readKey()
 	{
 		FILE *f = fopen( KEY_FILE_NAME, "r+b" );
 		if (f != NULL)
 		{
-			char *firstKeyParts, *secondKeyParts;
 			if (keyFileSizeIs128Bits(f))
-				fscanf_s(f, "%s%s", firstKeyParts, secondKeyParts);
+				fscanf_s(f, "%lu%lu", &firstKeyPart, &secondKeyPart);
 			else
 				ERROR_THROW(104);
 		}
 		else
 			ERROR_THROW(110);
 		fclose(f);
-	}
-
-	TKey stringToInt64(char* str)
-	{
-		TKey key = 0;
-		int digit = 0;
-		for (int i = strlen(str); i >= 0; i--)
-		{
-			
-		}
 	}
 
 	bool keyFileSizeIs128Bits(FILE *f)
@@ -40,10 +30,9 @@ namespace KEYS
 
 	void rotateKey(int firstKeyPart, int secondKeyPart, int shift = 25)
 	{
-		TKey temp1 = 0xFFFFFF8000000000;
-		TKey temp2 = 0xFFFFFF8000000000;
-		temp1 &= firstKeyPart;
-		temp2 &= secondKeyPart;
+		TKey temp1, temp2;
+		temp1 = firstKeyPart & 0xFFFFFF8000000000;
+		temp2 = secondKeyPart & 0xFFFFFF8000000000;
 		firstKeyPart <<= shift;
 		secondKeyPart <<= shift;
 		temp2 >>= sizeof(temp2) - shift;
@@ -51,4 +40,41 @@ namespace KEYS
 		firstKeyPart += temp2;
 		secondKeyPart += temp1;
 	}
+
+	TSubkey *getSubkey(TKey firstKeyPart, TKey secondKeyPart)
+	{
+		TSubkey argv[8];
+		TKey subkey = 0xFFFF;
+		for (int i = 0; i < 4; i++)
+			argv[i] = (subkey << (i * sizeof(argv[i])) & firstKeyPart);
+		for (int i = 4; i < 8; i++)
+			argv[i] = (subkey << (i * sizeof(argv[i]) % 4) & secondKeyPart);
+		return argv;
+	}
+
+	void initializeKeySet(TSubkey *keySet, int setSize)
+	{
+		int i = 0;
+		int loopCount = (sizeof(TKey) / sizeof(TSubkey)) >> 1;
+		TKey subkey = 0xFFFF;
+		while (true)
+		{
+			for (int j = 0; j < loopCount; j++)
+			{
+				keySet[i] = (subkey << (i / sizeof(TSubkey)) & firstKeyPart);
+				i++;
+			}
+			if (i < setSize)
+				for (int j = 0; j < loopCount; j++)
+				{
+					keySet[i] = (subkey << (i / sizeof(TSubkey)) & secondKeyPart);
+					i++;
+				}
+			else
+				break;
+		}
+	}	
 }
+
+int main(){}
+
--- KeyGen.h
+++ KeyGen.h
@@ -3,13 +3,17 @@
 
 #define KEY_LENGTH 128
 #define KEY_FILE_NAME "key.ini"
-#define SUBKEY_SET_SIZE 52
+#define SUBKEY_SET_SIZE 52        
 
-namespace KEYS
+namespace Keys
 {
-	unsigned __int64 firstKeyPart, secondKeyPart;
+	typedef unsigned long int TKey;
+	typedef unsigned short int TSubkey;
+	TKey firstKeyPart, secondKeyPart;
+	TSubkey keySet[SUBKEY_SET_SIZE];
 
 	void readKey();
 	void rotateKey(int firstKeyPart, int secondKeyPart, int shift);
+	void initializeKeySet(TKey *keySet, int setSize);
 	bool keyFileSizeIs128Bits(FILE *f);
 }
