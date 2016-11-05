0439c975bec8eec72b6f934ecc7005a349c5d1ca
Alex Radevich <virtuain98@gmail.com>
2016-11-05 21:10:33 +0300

    Keygen unit finished

--- KeyGen.cpp
+++ KeyGen.cpp
@@ -3,78 +3,135 @@
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
+#include <math.h>
 #include "Error.h"
 
 namespace Keys
 {
-	void readKey()
+	//функция открывает файл с 16-байтным ключом для чтения в бинарном формате.
+	//Возвращает 0, если работа завершена успешно, иначе - код ошибки
+	int readKey()
 	{
-		FILE *f = fopen( KEY_FILE_NAME, "r+b" );
+		FILE *f = fopen( KEY_FILE_NAME, "rb" );
+		//проверка на наличие файла
 		if (f != NULL)
 		{
-			if (keyFileSizeIs128Bits(f))
-				fscanf_s(f, "%lu%lu", &firstKeyPart, &secondKeyPart);
+			//проверка размера файла-ключа (16 байт)
+			if (keyFileSizeIsCorrect(f, KEY_LENGTH))
+				increaseKey(f, &firstKeyPart, &secondKeyPart);
 			else
+			{
+				//генерация ошибки, в случае несоответствия размера файла
 				ERROR_THROW(104);
+				return 104;
+			}
 		}
 		else
+		{
+			//генерация ошибки в случае, когда файл не открыт
 			ERROR_THROW(110);
+			return 110;
+		}
 		fclose(f);
+		return 0;
 	}
 
-	bool keyFileSizeIs128Bits(FILE *f)
+	//Процедура посимвольно читает символьный ключ из файла и преобразует в числовой формат
+	void increaseKey(FILE *f, TKey *firstKeyPart, TKey *secondKeyPart)
+	{
+		char num;
+		int i = 0;
+		TKey *target;
+		//посимвольное чтение из файла, пока не достигнут конец или функция fscan_f выдала ошибку
+		while ((!feof(f)) & (1 == fscanf_s(f, "%c", &num)))
+		{
+			if (i < 8)
+				target = firstKeyPart;
+			else
+				target = secondKeyPart;
+			//сдвигаем число влево на количество бит, равное размеру char
+			*target <<= 8;
+			//прибавляем прочитанный символ к числу
+			*target += num;
+			i++;
+		}
+	}
+
+	//функций, проверяющая размер файла. Возвращаемые значения:
+	//true - размер соответсвует эталону
+	//false - размер отличается от эталонного
+	bool keyFileSizeIsCorrect(FILE *f, unsigned int fileLength)
 	{
 		fseek(f, 0L, SEEK_END);
-		return (ftell(f) == round(KEY_LENGTH / 8));
+		bool result = (ftell(f) == fileLength >> 3);
+		fseek(f, 0, SEEK_SET);
+		return result;
 	}
 
-	void rotateKey(int firstKeyPart, int secondKeyPart, int shift = 25)
+	//Процедура побитового сдвига 128-битного ключа (по умолчанию на 25 бит)
+	void rotateKey(TKey *firstKeyPart, TKey *secondKeyPart, int shift = 25)
 	{
+		//*поскольку размер ключа - 16 байт (128) бит, а максимальный размер целочисленной переменной в данном проекте 8 байт (64 бит),
+		//*то весь ключ разделён на две 8-байтные части: firstKeyPart, secondKeyPart - над которыми и выполняются преобразования 
 		TKey temp1, temp2;
-		temp1 = firstKeyPart & 0xFFFFFF8000000000;
-		temp2 = secondKeyPart & 0xFFFFFF8000000000;
-		firstKeyPart <<= shift;
-		secondKeyPart <<= shift;
-		temp2 >>= sizeof(temp2) - shift;
-		temp1 >>= sizeof(temp1) - shift;
-		firstKeyPart += temp2;
-		secondKeyPart += temp1;
+		//сохранение первых shift бит каждой из двух частей ключа
+		temp1 = *firstKeyPart & 0xFFFFFF8000000000;
+		temp2 = *secondKeyPart & 0xFFFFFF8000000000;
+		//сдвиг на shift бит ключа влево
+		*firstKeyPart <<= shift;
+		*secondKeyPart <<= shift;
+		int rest = KEY_LENGTH / 2 - shift;
+		//сдвиг сохранённых shift бит ключа в конец
+		temp2 >>= rest;
+		temp1 >>= rest;
+		//дополнение младших разрядов сдвинутого на shift бит ключа сохранёнными ранее shift старшими битами
+		*firstKeyPart += temp2;
+		*secondKeyPart += temp1;
 	}
 
-	TSubkey *getSubkey(TKey firstKeyPart, TKey secondKeyPart)
+	//функция возвращает 2-байтный подключ, полученный из основного ключа
+	TSubkey getSubkey(TKey key, int index)
 	{
-		TSubkey argv[8];
-		TKey subkey = 0xFFFF;
-		for (int i = 0; i < 4; i++)
-			argv[i] = (subkey << (i * sizeof(argv[i])) & firstKeyPart);
-		for (int i = 4; i < 8; i++)
-			argv[i] = (subkey << (i * sizeof(argv[i]) % 4) & secondKeyPart);
-		return argv;
+		int shift = index * SUBKEY_SIZE;
+		int restShift = KEY_LENGTH / 2 - shift - SUBKEY_SIZE;
+		//шаблон для побитовой конъюнкции (требуемые 2 байта заполнены единицами, остальные - нули)
+		TKey subkeyTemplate = (SUBKEY_SHIFT_TEMPLATE >> shift);
+		//в key получаем два нужных байта подключа
+		key &= subkeyTemplate;
+		//сдвиг 2-ух полученных байт в крайнюю праую позицию и приведение типа к TSubkey
+		return (TSubkey)(key >>= restShift);
 	}
 
-	void initializeKeySet(TSubkey *keySet, int setSize)
+	//Процедура инициализации таблицы подключей
+	void initializeKeySet(TKey firstKeyPart, TKey secondKeyPart, TSubkey *keySet, int setSize = 52)
 	{
-		int i = 0;
-		int loopCount = (sizeof(TKey) / sizeof(TSubkey)) >> 1;
-		TKey subkey = 0xFFFF;
-		while (true)
+		TKey *target;
+		for (int i = 1; i <= SUBKEY_SET_SIZE; i++)
 		{
-			for (int j = 0; j < loopCount; j++)
-			{
-				keySet[i] = (subkey << (i / sizeof(TSubkey)) & firstKeyPart);
-				i++;
-			}
-			if (i < setSize)
-				for (int j = 0; j < loopCount; j++)
-				{
-					keySet[i] = (subkey << (i / sizeof(TSubkey)) & secondKeyPart);
-					i++;
-				}
+			//выбираем с какой из двух частей ключа работать
+			if ((i - 1) % 8 < 4)
+				target = &firstKeyPart;
 			else
-				break;
+				target = &secondKeyPart;
+			//инициализируем подключ
+			keySet[i - 1] = getSubkey(*target, (i - 1) % 4);
+			//если прошло 8 итерации, то делаем циклический сдвиг ключа (по умолчанию на 25 позиций)
+			if (i % 8 == 0)
+				rotateKey(&firstKeyPart, &secondKeyPart);
 		}
 	}	
-}
 
-int main(){}
+	//функция инициализации ключей. Возвращает 0, если инициализация прошла успешно, иначе - код ошибки
+	int initialize()
+	{
+		//чтение ключа из файла
+		int retCode = readKey();
+		//если чтение ключа из файла прошло успешно, то происходит генерация подключей
+		if (0 == retCode)
+			initializeKeySet(firstKeyPart, secondKeyPart, keySet);
+		else
+			return 110;
+		return 0;
+	}
+}
 
--- KeyGen.h
+++ KeyGen.h
@@ -1,19 +1,27 @@
 #pragma once
 #include <fstream>
 
-#define KEY_LENGTH 128
-#define KEY_FILE_NAME "key.ini"
-#define SUBKEY_SET_SIZE 52        
+#define KEY_LENGTH 128										//длина ключа
+#define KEY_FILE_NAME "key.txt"								//им€ файла-ключа
+#define SUBKEY_SET_SIZE 52									//количество подключей		
+#define SUBKEY_SIZE 16										//размер подключей
+#define SUBKEY_SHIFT_TEMPLATE 0xFFFF000000000000			//шаблон дл€ получени€ 2-байтной части из 8-байтного числа
 
 namespace Keys
 {
-	typedef unsigned long int TKey;
+	typedef unsigned _int64 TKey;							
 	typedef unsigned short int TSubkey;
-	TKey firstKeyPart, secondKeyPart;
+	//поскольку размер ключа - 16 байт (128) бит, а максимальный размер целочисленной переменной в данном проекте 8 байт (64 бит),
+	//то весь ключ разделЄн на две 8-байтные части, над которыми и выполн€ютс€ преобразовани€
+	TKey firstKeyPart = 0,
+		 secondKeyPart = 0;
 	TSubkey keySet[SUBKEY_SET_SIZE];
 
-	void readKey();
-	void rotateKey(int firstKeyPart, int secondKeyPart, int shift);
-	void initializeKeySet(TKey *keySet, int setSize);
-	bool keyFileSizeIs128Bits(FILE *f);
+	bool keyFileSizeIsCorrect(FILE *f, unsigned int fileSize);
+	int readKey();
+	int initialize();
+	void rotateKey(TKey *firstKeyPart, TKey *secondKeyPart, int shift);
+	void initializeKeySet(TKey firstKeyPart, TKey secondKeyPart, TSubkey *keySet, int setSize);
+	void increaseKey(FILE *f, TKey *firstKeyPart, TKey *secondKeyPart);
+	TSubkey getSubkey(TKey key, int index);
 }
